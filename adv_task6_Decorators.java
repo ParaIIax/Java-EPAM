b66d7b08415ff6fc9b9589bf0934ca6b46edd7bf
ParaIIax <93740558+ParaIIax@users.noreply.github.com>
2022-05-04 14:04:05 +0300

    Create adv_task6_Decorators.java

---
+++ adv_task6_Decorators.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Decorators {
+    public static List<String> evenIndexElementsSubList(List<String> sourceList) {
+        List<String> evenIndicesList = new ArrayList<>();
+        for (int i = 0; i < sourceList.size(); ++i) {
+            if (i % 2 == 0)
+                evenIndicesList.add(sourceList.get(i));
+        }
+        return evenIndicesList;
+    }
+
+}
