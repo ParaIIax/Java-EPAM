dfa1e92d7049a846f9c4e32bb54107643afb45cc
ParaIIax <93740558+ParaIIax@users.noreply.github.com>
2022-05-24 22:03:38 +0300

    Create adv_task5_part10_Company.java

---
+++ adv_task5_part10_Company.java
@@ -0,0 +1,14 @@
+package ua.advanced.task5.part10;
+
+public class Company {
+    private String companyName;
+
+    public Company(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+}
