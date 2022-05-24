888a62239041405681ebbb8f672f7b6e8ebc2569
ParaIIax <93740558+ParaIIax@users.noreply.github.com>
2022-05-24 22:01:52 +0300

    Create adv_task5_part9_Program.java

---
+++ adv_task5_part9_Program.java
@@ -0,0 +1,11 @@
+package ua.advanced.task5.part9;
+
+public class Program {
+    public static void main(String[] args) {
+        int numberOfParticipants = 5;
+        Lot[] lots = { new Lot(numberOfParticipants, 0,100), new Lot(numberOfParticipants, 1,30), new Lot(numberOfParticipants, 2,125) };
+        Auction auction = new Auction(lots, numberOfParticipants);
+        auction.startAuction();
+    }
+
+}
