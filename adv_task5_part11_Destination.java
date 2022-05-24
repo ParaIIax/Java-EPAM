c69c44c8792f3812b4e1855d4bc5554a4549a7f4
ParaIIax <93740558+ParaIIax@users.noreply.github.com>
2022-05-24 22:07:25 +0300

    Create adv_task5_part11_Destination.java

---
+++ adv_task5_part11_Destination.java
@@ -0,0 +1,25 @@
+package ua.advanced.task5.part11;
+
+public class Destination {
+    private String city;
+    private PlaneRangeEnum range;
+
+    public Destination(String city, PlaneRangeEnum range) {
+        this.city = city;
+        this.range = range;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public PlaneRangeEnum getRange() {
+        return range;
+    }
+
+    @Override
+    public String toString() {
+        return Destination.class.getSimpleName() + ": city - " + city + ", range - " + range;
+    }
+
+}
