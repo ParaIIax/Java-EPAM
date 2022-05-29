9b8c3b51d0547a0331f245eb26b6a71520930d78
ParaIIax <93740558+ParaIIax@users.noreply.github.com>
2022-05-29 18:12:41 +0300

    Create adv_task8_Entities_Film.java

---
+++ adv_task8_Entities_Film.java
@@ -0,0 +1,39 @@
+package ua.advanced.task8.Enities;
+
+//Film
+public class Film extends Entity {
+    private String name;
+    private int release_date;
+    private String country_name;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getReleaseDate() {
+        return release_date;
+    }
+
+    public String getCountryName() {
+        return country_name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setReleaseDate(int release_date) {
+        this.release_date = release_date;
+    }
+
+    public void setCountryName(String country_name) {
+        this.country_name = country_name;
+    }
+
+    @Override
+    public String toString() {
+        return Film.class.getSimpleName() + ": " + "id - " + getId() + ", date of release - "
+                + release_date + ", the name of the country - " + country_name;
+    }
+
+}
