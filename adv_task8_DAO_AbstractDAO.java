a7c6f1384a94cc08be62d896728c65fcb1ec7dac
ParaIIax <93740558+ParaIIax@users.noreply.github.com>
2022-05-29 18:08:27 +0300

    Create adv_task8_DAO_AbstractDAO.java

---
+++ adv_task8_DAO_AbstractDAO.java
@@ -0,0 +1,45 @@
+package ua.advanced.task8.DAO;
+
+import ua.advanced.task8.ConnectionPool.BasicConnectionPool;
+import ua.advanced.task8.Enities.Entity;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+public abstract class AbstractDAO <K, T extends Entity> {
+    public abstract List<T> findAll();
+    public abstract T findEntityById(K id);
+    public abstract boolean delete(K id);
+    public abstract boolean delete(T entity);
+    public abstract boolean create(T entity);
+    public abstract T update(T entity);
+
+    protected BasicConnectionPool connectionPool;
+
+    public AbstractDAO(BasicConnectionPool connectionPool) {
+        this.connectionPool = connectionPool;
+    }
+
+    public void close(Statement st) {
+        try {
+            if (st != null) {
+                st.close();
+            }
+        } catch (SQLException e) {
+            //log about the impossibility of closing the Statement
+        }
+    }
+
+    public void close(Connection connection) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            //throwing an exception, because pool is broken
+        }
+    }
+
+}
