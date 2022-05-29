2cb830958b74f363e16f3083b036744fddde717b
ParaIIax <93740558+ParaIIax@users.noreply.github.com>
2022-05-29 18:11:21 +0300

    Create adv_task8_Entities_Actor.java

---
+++ adv_task8_Entities_Actor.java
@@ -0,0 +1,30 @@
+package ua.advanced.task8.Enities;
+
+//Actor
+public class Actor extends Entity {
+    private String full_name;
+    private int birth_date;
+
+    public String getFullName() {
+        return full_name;
+    }
+
+    public void setFullName(String full_name) {
+        this.full_name = full_name;
+    }
+
+    public int getBirthDate() {
+        return birth_date;
+    }
+
+    public void setBirthDate(int birth_date) {
+        this.birth_date = birth_date;
+    }
+
+    @Override
+    public String toString() {
+        return Actor.class.getSimpleName() + ": id - " + getId() + ", full name - " +
+                full_name + ", date of birth - " + birth_date;
+    }
+
+}
