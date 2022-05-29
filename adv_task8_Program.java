bb530a48d7a4b2196ca663c75a694bb8379e5892
ParaIIax <93740558+ParaIIax@users.noreply.github.com>
2022-05-29 18:14:13 +0300

    Create adv_task8_Program.java

---
+++ adv_task8_Program.java
@@ -0,0 +1,20 @@
+package ua.advanced.task8;
+
+import ua.advanced.task8.ConnectionPool.BasicConnectionPool;
+import ua.advanced.task8.DAO.ActorDAO;
+import ua.advanced.task8.DAO.FilmDAO;
+
+import java.sql.SQLException;
+
+//Program
+public class Program {
+    public static void main(String args[]) throws SQLException {
+        var connectionPool = BasicConnectionPool.create(
+                "jdbc:mysql://localhost:3306/videolibrary", "root", "root");
+        FilmDAO filmDAO = new FilmDAO(connectionPool);
+        ActorDAO actorDAO = new ActorDAO(connectionPool);
+        Menu menu = new Menu(actorDAO, filmDAO);
+        menu.startMenu();
+    }
+
+}
