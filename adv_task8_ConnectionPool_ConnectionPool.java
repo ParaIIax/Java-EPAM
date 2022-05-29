12fb5dc22202fc1a3daf7755a13505bceaafea70
ParaIIax <93740558+ParaIIax@users.noreply.github.com>
2022-05-29 18:05:37 +0300

    Create adv_task8_ConnectionPool_ConnectionPool.java

---
+++ adv_task8_ConnectionPool_ConnectionPool.java
@@ -0,0 +1,11 @@
+package ua.advanced.task8.ConnectionPool;
+
+import java.sql.Connection;
+
+public interface ConnectionPool {
+    Connection getConnection();
+    boolean releaseConnection(Connection connection);
+    String getUrl();
+    String getUser();
+    String getPassword();
+}
